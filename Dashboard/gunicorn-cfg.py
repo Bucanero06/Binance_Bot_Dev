0aa2216b4f0b20c23685da8f479b8f702b40208a
ruben <ruben@moonshot.codes>
2023-02-14 13:14:14 -0500

    .

--- Dashboard/gunicorn-cfg.py
+++ Dashboard/gunicorn-cfg.py
@@ -6,3 +6,6 @@ bind = '0.0.0.0:8000'
 # loglevel = 'debug'
 # capture_output = True
 # enable_stdio_inheritance = True
+
+
+# web: gunicorn -b 0.0.0.0:8000 --workers 1 --threads 100 BOTs.run_bot:app --log-file=-
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:8000 --workers 1 --threads 100 BOTs.run_bot:app --log-file=-
+web: gunicorn -b 0.0.0.0:8000  BOTs.run_bot:app --log-file=-
